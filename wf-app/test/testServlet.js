546d04a2ac8ee793fca7957054c5ae704ef374af
fyhao <fyhao1@gmail.com>
2018-05-28 17:14:46 +0800

    #21 basic test case executeTestCase

--- wf-app/test/testServlet.js
+++ wf-app/test/testServlet.js
@@ -3,13 +3,14 @@ var modServlet = require('../lib/module/engine/modServlet');
 var eventMgr = {};
 describe('modServlet module', function () {
   describe('createHandler', function() {
-	  it('should return status 0 when deploy with json conf', function test(done) {
+	  it('should be able to get response for simple handler', function test(done) {
 		var appItem = {
 			app:'test',
 			flows:{
 				flow_1: {
 					steps : [
 						{type:'log',message:'test'},
+						{type:'response',body:'test'},
 					]
 				}
 			}
@@ -22,7 +23,7 @@ describe('modServlet module', function () {
 		};
 		var res = {
 			end : function(body) {
-				
+				assert.equal(body, "test")
 			}
 		}
 		eventMgr.trigger = function(name, opts) {
@@ -35,6 +36,49 @@ describe('modServlet module', function () {
 		var handler = modServlet.createHandler(eventMgr, appItem, appLi);
 		handler(req, res);
 		
+	  });
+	  var executeTestCase = function(opts) {
+		  var appItem = {
+				app:'test',
+				flows:opts.flows
+			}
+			var appLi = { flow: opts.entryFlow};
+			var req = {
+				params : opts.requestParams,
+				param : function(name) {
+					return this.params[name]
+				}
+			};
+			var res = {
+				end : opts.resEnd
+			}
+			eventMgr.trigger = function(name, opts1) {
+				if(name == 'flowExecutedDone') {
+					var ctx = opts1.ctx;
+					opts.done();
+				}
+			}
+			modServlet._injectUnitTest({apps:[appItem]});
+			var handler = modServlet.createHandler(eventMgr, appItem, appLi);
+			handler(req, res);
+	  }
+	  it('should be OK for executeTestCase function structure', function test(done) {
+		  executeTestCase({
+			  flows:{
+				flow_1: {
+					steps : [
+						{type:'log',message:'test'},
+						{type:'response',body:'test'},
+					]
+				}
+			  },
+			  entryFlow:'flow_1',
+			  requestParams : {name:'ali',age:20},
+			  done:done,
+			  resEnd : function(body) {
+				  assert.equal(body,"test")
+			  }
+		  });
 	  });
   });
   
