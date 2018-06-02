1f3639e1c83a28c2f54cf8617116359ff607f4ba
fyhao <fyhao1@gmail.com>
2018-06-02 22:51:04 +0800

    basic listener view

--- wf-admin/src/components/AppEditForm.js
+++ wf-admin/src/components/AppEditForm.js
@@ -5,6 +5,7 @@ import Constants from './Constants';
 import StandardForm from './StandardForm';
 import NavButton from './NavButton';
 import AppList from './AppList';
+import ListView from './ListView';
 import ee from './EventManager';
 class AppEditForm extends Component {
   constructor(opts) {
@@ -36,7 +37,6 @@ class AppEditForm extends Component {
 				})
 		  }
 	  };
-	  
 	  this.componentWillMount = this.componentWillMount.bind(this);
 	  this.componentWillUnmount = this.componentWillUnmount.bind(this);
 	  this.onEditForm = this.onEditForm.bind(this);
@@ -61,6 +61,8 @@ class AppEditForm extends Component {
 	  })
 	  this.setState({options:options});
 	  this.row = opts.row;
+	  
+	  this.requestListener()
   }
   
   handleDelete() {
@@ -77,13 +79,36 @@ class AppEditForm extends Component {
 		})
   }
   
+  requestListener() {
+	  var me = this;
+	  axios({
+		  method: 'GET',
+		  url: Constants.API_URL + '/app/' + this.row.name + '/listener',
+		  data: {
+		  }
+		}).then(response => {
+			me.setState({listenerData:response.data.listeners});
+		})
+  }
+  state = {
+	 listenerData:[]
+  }
   render() {
+	var options = {};
+	
+	options.data = this.state.listenerData;
+	options.fields = [
+		{heading:'Type',key:'type'},
+		{heading:'Endpoint',key:'endpoint'}
+	];
 	
     return (
       <div>
-		<NavButton onClick={this.handleDelete} title="Delete"/>
+		<NavButton onClick={this.handleDelete} title="Delete App"/>
 		<StandardForm options={this.state.options} />
-		<p>DEBUG: {this.state.submittedFields}</p>
+		<h3>Listeners</h3>
+		<NavButton onClick={this.handleDelete} title="Create Listener"/>
+		<ListView options={options}/>
 	  </div>
 	  
     );
