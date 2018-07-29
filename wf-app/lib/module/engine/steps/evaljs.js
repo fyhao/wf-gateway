2f0399c212b0f63ea24d4b8981b9188728f01a03
fyhao <fyhao1@gmail.com>
2018-07-29 11:42:13 +0800

    #39 wf-app evaljs

---
+++ wf-app/lib/module/engine/steps/evaljs.js
@@ -0,0 +1,24 @@
+var util = ProjRequire('lib/MyUtil');
+module.exports = {
+	
+	process : function(ctx, step, checkNext) {
+		if(typeof step.var != 'undefined' && step.code.indexOf('return') == -1) {
+			step.code = 'return ' + step.code;
+		}
+		var isNextWrapperCalled = false;
+		var nextWrapper = function() {
+			if(isNextWrapperCalled) return;
+			process.nextTick(checkNext);
+			isNextWrapperCalled = true;
+		}
+		
+		var val = new Function('vars', 'ctx', 'util', 'next', 'vars; ctx; util; next; ' + step.code);
+		ctx.vars[step.var] = val(ctx.vars, ctx, util, nextWrapper);
+		if(typeof step.timeout != 'undefined') {
+			setTimeout(nextWrapper, step.timeout);
+		}
+		else {
+			process.nextTick(checkNext);
+		}
+	}
+}
