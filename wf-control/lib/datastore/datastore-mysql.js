189b9606d5f7e387accd3dab5df77591b2e72eff
fyhao <fyhao1@gmail.com>
2018-08-22 17:53:50 +0800

    datastore mysql import listener app bug

--- wf-control/lib/datastore/datastore-mysql.js
+++ wf-control/lib/datastore/datastore-mysql.js
@@ -516,7 +516,9 @@ var DataStoreMysql = function(dbcfg) {
 				}
 				batches.push({sql:'delete from listener'});
 				for(var app in input.listenerData) {
-					input.listenerData[app].app = app;
+					for(var i = 0; i < input.listenerData[app].length; i++) {
+						input.listenerData[app][i].app = app;
+					}
 					batches.push({sql:'insert into listener SET ?', fields:input.listenerData[app]});
 				}
 				batches.push({sql:'delete from instance'});
